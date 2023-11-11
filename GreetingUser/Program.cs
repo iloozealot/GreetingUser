5dc141d03528941846d3e851b10bf896b7f04d98
iloozealot <iloozealot@gmail.com>
2023-11-11 13:15:14 -0600

    Add project files.

---
+++ GreetingUser/Program.cs
@@ -0,0 +1,13 @@
+﻿// GreetingUser
+//Author: Justin Spencer
+//In this application, we will welcome the user to our application and then we will ask them their first name. We will greet them by name, then ask their favorite color and respond.
+
+
+Console.WriteLine("Hello, welcome to my application.");
+Console.WriteLine("What is your first name?");
+string? userName = Console.ReadLine();
+//Console.WriteLine("Thank you for using my application " + username + ".");
+Console.WriteLine($"Thank you for using my application {userName}.");
+Console.WriteLine($"{userName}, what is you favorite color?");
+string? userColor = Console.ReadLine();
+Console.WriteLine($"{userColor} is my favorite too!");
